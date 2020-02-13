e25bfc20d40563cad8076e4af7ee247f8fbb98f8
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2020-02-12 23:48:10 -0600

    added comments to consider

--- lib/logic.ml
+++ lib/logic.ml
@@ -1,9 +1,9 @@
 type proposition = unit -> bool
 and compound = 
-  | False
+  | False (* Do we want this to instead be represented as `Prop (() -> false)`, as specified in the line below? *)
   | Prop of proposition
   | And of compound * compound
   | Implies of compound * compound
   | Or of compound * compound
-  | Iff of compound * compound
+  | Iff of compound * compound (* Do we want to be pedantic about this? 'Iff' can be constructed from and/implies *)
   | Not of compound;;
