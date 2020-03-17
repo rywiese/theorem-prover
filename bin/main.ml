3c1b77d2204da22ef97ef93cef13432ab4f69231
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2020-03-16 22:54:42 -0500

    updated

--- bin/main.ml
+++ bin/main.ml
@@ -4,4 +4,6 @@ let () =
   let result = Math.add 2 3 in
   print_endline (string_of_int result);
   let result = Math.sub 3 1 in
-  print_endline (string_of_int result)
+  print_endline (string_of_int result);
+  let result = Logic.Prop ("test") in
+  print_endline (Logic.formula_to_string result);
--- lib/logic.ml
+++ lib/logic.ml
@@ -1,9 +1,9 @@
-type proposition = unit -> bool
-and compound = 
-  | False (* Do we want this to instead be represented as `Prop (() -> false)`, as specified in the line below? *)
+type proposition = string
+and formula =
+  | False
   | Prop of proposition
-  | And of compound * compound
-  | Implies of compound * compound
-  | Or of compound * compound
-  | Iff of compound * compound (* Do we want to be pedantic about this? 'Iff' can be constructed from and/implies *)
-  | Not of compound;;
+  | And of formula * formula
+  | Implies of formula * formula
+  | Or of formula * formula
+  | Iff of formula * formula
+  | Not of formula;;
