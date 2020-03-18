7b46a0379dbe06f69281f928446101e4a5fb85b9
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2020-03-17 22:47:55 -0500

    cleaned up main.ml so that it runs

--- bin/main.ml
+++ bin/main.ml
@@ -5,5 +5,3 @@ let () =
   print_endline (string_of_int result);
   let result = Math.sub 3 1 in
   print_endline (string_of_int result);
-  let result = Logic.Prop ("test") in
-  print_endline (Logic.formula_to_string result);
